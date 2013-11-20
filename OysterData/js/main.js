ab4b69742ddddbfb4a6841aa5e0f0f964c676b4b
Kevin Marsh <kevinmarsh3@gmail.com>
2013-11-20 20:37:50 +0000

    moving all js out of html file

--- OysterData/js/main.js
+++ OysterData/js/main.js
@@ -1,6 +1,3 @@
-var reader = new FileReader();
-
-
 function readUpload(reqFile){
     if(reqFile.files && reqFile.files[0]){
         var reader = new FileReader();
@@ -29,20 +26,22 @@ function processCSV(csvData) {
     return [headerRow, rows];
 }
 
-// function sortKeys(obj) {
-//     var dictKeys = [];
-//     for(var key in obj) {
-//         if (obj.hasOwnProperty(key)) {
-//             dictKeys.push(key);
-//         }
-//     }
-//     var sortedArray = dictKeys.sort();
-//     sortedKeys = [];
-//     for (var k in sortedArray) {
-//         sortedKeys.push(sortedArray[k]);
-//     }
-//     return sortedKeys;
-// }
+function saveJourneyJSON (data) {
+    // TODO: check that journeys aren't already set in sessionStorage
+
+    // var journeys = sessionStorage.getItem('journeys');
+    // if (journeys) {
+    //     journeys = JSON.parse(jsonData);
+    // } else {
+    //     journeys = [];
+    // }
+    // journeys += jsonData;
+    // sessionStorage.setItem('journeys', JSON.stringify(journeys));
+    if (typeof data !== 'string') {
+        data = JSON.stringify(data);
+    }
+    sessionStorage.setItem('journeys', data);
+}
 
 function printJourneyJSON (jsonData) {
     var journeys = {};
@@ -73,3 +72,92 @@ $('button.upload').click(function() {
 $('input[type="file"]').change(function () {
     readUpload(this);
 });
+
+
+/*******************************************************************************
+*******************************************************************************/
+
+function loadSampleData() {
+    $.getJSON("../data/oyster-data.json", function(jsonData) {
+        // SAMPLE DATA IN JSON
+        // Balance: "9.95"
+        // Charge: "2.8"
+        // Credit: ""
+        // Date: "13-Aug-2013"
+        // End Time: "17:59"
+        // Journey/Action: "Brixton [London Underground] to Camden Town"
+        // Note: ""
+        // Start Time: "17:36"
+
+        // == {"Journey/Action": "Brixton [London Underground] to Camden Town", "Start Time": "17:36", "Charge": "2.8", "Note": "", "Credit": "", "End Time": "17:59", "Date": "13-Aug-2013", "Balance": "9.95"},
+
+        routes = convert_journeys_to_routes(jsonData);
+        convert_stations_to_table(routes, 2);
+    });
+}
+
+function convert_journeys_to_routes (journeys) {
+    // Take the array of individual journeys and convert them to routes with aggregated data
+    routes = {};  // Station name, number of journeys, average cost, average time
+    $.each(journeys, function(i, row) {
+        if (parseFloat(row['Charge'])) {
+            // Skip any ones that are bus journeys or top ups
+            var stationname = row['Journey/Action'];
+            if (!(stationname in routes)) {
+                // Add the station name to the routes
+                routes[stationname] = {
+                    'count': 0,
+                    'time': 0,
+                    'price': 0
+                };
+            }
+            var timeDiff = new Date(2013, 01, 01, row['End Time'].split(':')[0], row['End Time'].split(':')[1], 0, 0) - new Date(2013, 01, 01, row['Start Time'].split(':')[0], row['Start Time'].split(':')[1], 0, 0);
+            routes[stationname]['count'] += 1;
+            routes[stationname]['time'] += timeDiff;
+            routes[stationname]['price'] += parseFloat(row['Charge']);
+        }
+    });
+    return routes;
+}
+
+function strip_station_name(stationname) {
+    return stationname.replace(' [London Underground]', '<span title="London Underground">*</span>').replace(' [London Underground / National Rail]', '<span title="London Underground / National Rail">*</span>').replace(' [National Rail]', '<span title="National Rail">*</span>');
+}
+function convert_stations_to_table(stationData, minJourneys){
+    var stationList = $('<table/>').appendTo('body');
+    $('<tr/>').html('<th>Staion Name</th><th>Count</th><th>Avg Time</th><th>Avg Price</th>').appendTo(stationList);
+    $.each(stationData, function(station, data) {
+        // console.log(station, data);
+        if (!minJourneys || data['count'] >= minJourneys) {
+            var stationRow = $('<tr/>').appendTo(stationList);
+            $('<td/>').html(strip_station_name(station)).appendTo(stationRow);
+            $('<td/>').text(data['count']).appendTo(stationRow);
+            $('<td/>').text((data['time'] / 1000 / 60 / data['count']).toFixed(2)).appendTo(stationRow);
+            $('<td/>').text((data['price'] / data['count']).toFixed(2)).appendTo(stationRow);
+        }
+    });
+}
+$('#loadsampledata').on('click', loadSampleData);
+
+/*******************************************************************************
+*******************************************************************************/
+
+/*******************************************************************************
+
+* Upload the csv file via an upload button
+* Convert the csv file to JSON (in the format of an array of journeys)
+    {
+        "Journey/Action": "Brixton [London Underground] to Camden Town",
+        "Start Time": "17:36",
+        "Charge": "2.8",
+        "Note": "",
+        "Credit": "",
+        "End Time": "17:59",
+        "Date": "13-Aug-2013",
+        "Balance": "9.95"
+    }
+* Save the journeys in session/local storage
+* Process the journeys into routes (Station A to Station B)
+
+
+*******************************************************************************/
