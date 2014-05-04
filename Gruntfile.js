583c3d0d2a7f623bc0f17e0f9c9d9ee01f52966a
Kevin Marsh <kevinmarsh3@gmail.com>
2014-05-04 12:30:55 +0100

    switched to load-grunt-tasks to load grunt plugins

--- Gruntfile.js
+++ Gruntfile.js
@@ -72,10 +72,6 @@ module.exports = function(grunt) {
 
     });
 
-    grunt.loadNpmTasks('grunt-contrib-concat');
-    grunt.loadNpmTasks('grunt-contrib-uglify');
-    grunt.loadNpmTasks('grunt-contrib-imagemin');
-    grunt.loadNpmTasks('grunt-contrib-sass');
-    grunt.loadNpmTasks('grunt-contrib-watch');
+    require('load-grunt-tasks')(grunt);
     grunt.registerTask('default', ['sass', 'concat', 'uglify', 'imagemin']);
 };
