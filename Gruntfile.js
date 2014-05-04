51d16dd83218c0e057442eed5bf7f012d930bb4e
Kevin Marsh <kevinmarsh3@gmail.com>
2014-05-04 12:27:19 +0100

    added grunt watch command

--- Gruntfile.js
+++ Gruntfile.js
@@ -53,11 +53,29 @@ module.exports = function(grunt) {
             }
         },
 
+        watch: {
+            scripts: {
+                files: ['OysterData/js/*.js'],
+                tasks: ['concat:js', 'uglify'],
+                options: {
+                    spawn: false,
+                },
+            },
+            css: {
+                files: ['OysterData/css/*.scss'],
+                tasks: ['sass', 'concat:css'],
+                options: {
+                    spawn: false,
+                }
+            }
+        },
+
     });
 
     grunt.loadNpmTasks('grunt-contrib-concat');
     grunt.loadNpmTasks('grunt-contrib-uglify');
     grunt.loadNpmTasks('grunt-contrib-imagemin');
     grunt.loadNpmTasks('grunt-contrib-sass');
+    grunt.loadNpmTasks('grunt-contrib-watch');
     grunt.registerTask('default', ['sass', 'concat', 'uglify', 'imagemin']);
 };
